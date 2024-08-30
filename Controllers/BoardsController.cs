930282139d753c35738b18b3d588a34830ce568b
MieWesth <mieelisewesth@gmail.com>
2024-08-30 12:07:05 +0200

    Add project files.

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SurfsUp.Controllers
+{
+    public class BoardsController : Controller
+    {
+        public IActionResult Index()
+        {
+          
+            
+            return View();
+        }
+
+        
+    }
+}
---
+++ Models/Board.cs
@@ -0,0 +1,21 @@
+﻿namespace SurfsUp.Models
+{
+    public class Board
+    {
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Thickness { get; set; }
+
+        public double Volume { get; set; }
+
+        public string Type { get; set; }
+
+        public int Price { get; set; }
+
+        public string Equipment { get; set; }
+
+        public override string ToString() => $"Name: {Name}";
+
+    }
+}
